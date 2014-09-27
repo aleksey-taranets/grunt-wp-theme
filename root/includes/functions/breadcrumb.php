f4441708ba029ffe7ac1b0c5bd55772b32b600a5
Aleksey <markup.javascript@gmail.com>
2014-09-27 22:06:46 +0300

    breadcrumb

--- root/includes/functions/breadcrumb.php
+++ root/includes/functions/breadcrumb.php
@@ -1,42 +1,147 @@
 <?php
-function the_breadcrumb() {
+/**
+ * Dimox Breadcrumbs
+ * http://dimox.net/wordpress-breadcrumbs-without-a-plugin/
+ * Since ver 1.0
+ * Add this to any template file by calling dimox_breadcrumbs()
+ * Changes: MC added taxonomy support
+ */
+function the_breadcrumb(){
+    /* === OPTIONS === */
+    $text['home']     = 'Главная'; // text for the 'Home' link
+    $text['category'] = '%s'; // text for a category page
+    $text['tax'] 	  = '%s'; // text for a taxonomy page
+    $text['search']   = 'Результат поиска для "%s"'; // text for a search results page
+    $text['tag']      = '%s'; // text for a tag page
+    $text['author']   = '%s'; // text for an author page
+    $text['404']      = '404'; // text for the 404 page
+
+    $showCurrent = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
+    $showOnHome  = 0; // 1 - show breadcrumbs on the homepage, 0 - don't show
+    $delimiter   = ' </li><li> '; // delimiter between crumbs
+    $before      = '<span class="current">'; // tag before the current crumb
+    $after       = '</span>'; // tag after the current crumb
+    /* === END OF OPTIONS === */
+
     global $post;
-    $sep = '<li class="separator"> / </li>';
-    echo '<ul id="breadcrumbs">';
-    if (!is_home()) {
-        echo '<li><a href="';
-        echo get_option('home');
-        echo '">';
-        echo 'Главная';
-        echo '</a></li>'.$sep;
-        if (is_category() || is_single()) {
-            echo '<li>';
-            the_category(' </li>'.$sep.'<li> ');
-            if (is_single()) {
-                echo '</li>'.$sep.'<li>';
-                the_title();
-                echo '</li>';
+    $homeLink = get_bloginfo('url') . '/';
+    $linkBefore = '<span typeof="v:Breadcrumb">';
+    $linkAfter = '</span>';
+    $linkAttr = ' rel="v:url" property="v:title"';
+    $link = $linkBefore . '<a' . $linkAttr . ' href="%1$s">%2$s</a>' . $linkAfter;
+
+    if (is_home() || is_front_page()) {
+
+        if ($showOnHome == 1) echo '<ul class="breadcrumb"><li>
+            <a href="' . $homeLink . '">' . $text['home'] . '</a></li>';
+
+    } else {
+
+        echo '<ul class="breadcrumb" xmlns:v="http://rdf.data-vocabulary.org/#"><li>' . sprintf($link, $homeLink, $text['home']) . $delimiter;
+
+
+        if ( is_category() ) {
+            $thisCat = get_category(get_query_var('cat'), false);
+            if ($thisCat->parent != 0) {
+                $cats = get_category_parents($thisCat->parent, TRUE, $delimiter);
+                $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+                $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+                echo $cats;
             }
-        } elseif (is_page()) {
-            if($post->post_parent){
-                $anc = get_post_ancestors( $post->ID );
-                $title = get_the_title();
-                foreach ( $anc as $ancestor ) {
-                    $output = '<li><a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a></li>' . $sep ;
-                }
-                echo $output;
-                echo '<strong title="'.$title.'"> '.$title.'</strong>';
+            echo $before . sprintf($text['category'], single_cat_title('', false)) . $after;
+
+        } elseif( is_tax() ){
+            $thisCat = get_category(get_query_var('cat'), false);
+            if ($thisCat->parent != 0) {
+                $cats = get_category_parents($thisCat->parent, TRUE, $delimiter);
+                $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+                $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+                echo $cats;
+            }
+            echo $before . sprintf($text['tax'], single_cat_title('', false)) . $after;
+
+        }elseif ( is_search() ) {
+            echo $before . sprintf($text['search'], get_search_query()) . $after;
+
+        } elseif ( is_day() ) {
+            echo sprintf($link, get_year_link(get_the_time('Y')), get_the_time('Y')) . $delimiter;
+            echo sprintf($link, get_month_link(get_the_time('Y'),get_the_time('m')), get_the_time('F')) . $delimiter;
+            echo $before . get_the_time('d') . $after;
+
+        } elseif ( is_month() ) {
+            echo sprintf($link, get_year_link(get_the_time('Y')), get_the_time('Y')) . $delimiter;
+            echo $before . get_the_time('F') . $after;
+
+        } elseif ( is_year() ) {
+            echo $before . get_the_time('Y') . $after;
+
+        } elseif ( is_single() && !is_attachment() ) {
+            if ( get_post_type() != 'post' ) {
+                $post_type = get_post_type_object(get_post_type());
+                $slug = $post_type->rewrite;
+                printf($link, $homeLink . '/' . $slug['slug'] . '/', $post_type->labels->singular_name);
+                if ($showCurrent == 1) echo $delimiter . $before . get_the_title() . $after;
             } else {
-                echo '<li>'.get_the_title().'</li>';
+                $cat = get_the_category(); $cat = $cat[0];
+                $cats = get_category_parents($cat, TRUE, $delimiter);
+                if ($showCurrent == 0) $cats = preg_replace("#^(.+)$delimiter$#", "$1", $cats);
+                $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+                $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+                echo $cats;
+                if ($showCurrent == 1) echo $before . get_the_title() . $after;
             }
+
+        } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
+            $post_type = get_post_type_object(get_post_type());
+            echo $before . $post_type->labels->singular_name . $after;
+
+        } elseif ( is_attachment() ) {
+            $parent = get_post($post->post_parent);
+            $cat = get_the_category($parent->ID); $cat = $cat[0];
+            $cats = get_category_parents($cat, TRUE, $delimiter);
+            $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+            $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+            echo $cats;
+            printf($link, get_permalink($parent), $parent->post_title);
+            if ($showCurrent == 1) echo $delimiter . $before . get_the_title() . $after;
+
+        } elseif ( is_page() && !$post->post_parent ) {
+            if ($showCurrent == 1) echo $before . get_the_title() . $after;
+
+        } elseif ( is_page() && $post->post_parent ) {
+            $parent_id  = $post->post_parent;
+            $breadcrumbs = array();
+            while ($parent_id) {
+                $page = get_page($parent_id);
+                $breadcrumbs[] = sprintf($link, get_permalink($page->ID), get_the_title($page->ID));
+                $parent_id  = $page->post_parent;
+            }
+            $breadcrumbs = array_reverse($breadcrumbs);
+            for ($i = 0; $i < count($breadcrumbs); $i++) {
+                echo $breadcrumbs[$i];
+                if ($i != count($breadcrumbs)-1) echo $delimiter;
+            }
+            if ($showCurrent == 1) echo $delimiter . $before . get_the_title() . $after;
+
+        } elseif ( is_tag() ) {
+            echo $before . sprintf($text['tag'], single_tag_title('', false)) . $after;
+
+        } elseif ( is_author() ) {
+            global $author;
+            $userdata = get_userdata($author);
+            echo $before . sprintf($text['author'], $userdata->display_name) . $after;
+
+        } elseif ( is_404() ) {
+            echo $before . $text['404'] . $after;
         }
+
+        if ( get_query_var('paged') ) {
+            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
+            echo __('Page') . ' ' . get_query_var('paged');
+            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
+        }
+
+        echo '</li></ul>';
+
     }
-    elseif (is_tag()) {single_tag_title();}
-    elseif (is_day()) {echo"<li>Архивы за "; the_time('F jS, Y'); echo'</li>';}
-    elseif (is_month()) {echo"<li>Архивы за "; the_time('F, Y'); echo'</li>';}
-    elseif (is_year()) {echo"<li>Архивы за "; the_time('Y'); echo'</li>';}
-    elseif (is_author()) {echo"<li>Архивы автора"; echo'</li>';}
-    elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "<li>Архивы блога"; echo'</li>';}
-    elseif (is_search()) {echo"<li>Результаты поиска"; echo'</li>';}
-    echo '</ul>';
-}
+} // end dimox_breadcrumbs()
